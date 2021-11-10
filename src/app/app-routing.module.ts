9b1f577ec71dfdc57d380fda365c3ca585afdea9
Brant Hajek <branthajek@gmail.com>
2021-11-09 22:49:46 -0800

    layout updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { ContactComponent } from './contact/contact.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [
-  { path: '', component: AboutMeComponent },
+  { path: '', redirectTo: '/about-me', pathMatch: 'full'},
+  { path: 'about-me', component: AboutMeComponent },
   { path: 'portfolio', component: PortfolioComponent },
   { path: 'contact', component: ContactComponent }
 ];
