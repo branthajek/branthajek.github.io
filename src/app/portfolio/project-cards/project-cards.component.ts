f5d89f1d50d413bc2d66e8d84a1d474135f20ec1
Brant Hajek <branthajek@gmail.com>
2021-12-12 17:00:08 -0800

    minor updates

--- src/app/portfolio/project-cards/project-cards.component.ts
+++ src/app/portfolio/project-cards/project-cards.component.ts
@@ -32,7 +32,7 @@ export class ProjectCardsComponent {
     },
     { 
       title: 'Shopping List App', 
-      description: 'This is a pretty big, full-featured Angular app in all of its glory. It allows you to create an account and add recipes along with ingredients and images, description, etc. The ingredients can be added to the shopping list, and your recipes can be saved to the database.',
+      description: 'This is a pretty big, full-featured Angular app. It allows you to create an account and add recipes along with ingredients, an image, and description. The ingredients for each recipe can be added to the shopping list, and your recipes can be saved to the database.',
       codeDescription: 'Angular, NgRx, Bootstrap, Firebase back-end',
       viewBrowser: 'https://shopping-list-eb81d.web.app/auth',
       viewCode: 'https://github.com/branthajek/shopping-list',
@@ -41,7 +41,7 @@ export class ProjectCardsComponent {
     },
     { 
       title: 'Location Info App', 
-      description: 'I explore Typescript on its own and experiment with some RESTful API calls from Google Maps, as well as OpenWeather. This allows you to enter a location which will be marked on a google map widget with a pin, and some current information for the entered location will display including elevation, current temperature, and weather conditions.',
+      description: 'In plain Typescript, I used RESTful APIs from Google Maps and OpenWeatherMap. This app allows you to enter a location which will be marked on a google map widget with a pin, and some current information for the entered location will display including elevation, current temperature, and weather conditions.',
       codeDescription: 'Typescript, Axios, Google Maps JS API, OpenWeatherMap API, Bootstrap, Webpack',
       viewBrowser: 'https://location-info-3c68f.web.app/',
       viewCode: 'https://github.com/branthajek/location-info',
@@ -50,7 +50,7 @@ export class ProjectCardsComponent {
     },
     { 
       title: 'To-do List App', 
-      description: 'This is a simple app that explores the possibility of using React with Typescript, for type-safety and better IDE support. You can create a to-do item, and delete each one individually whenever they are complete.',
+      description: 'This is a simple app using React with Typescript, for type-safety and better IDE support. You can create a to-do item, and delete each one individually whenever they are complete.',
       codeDescription: 'React, Typescript',
       viewBrowser: 'https://to-do-list-react-ts.web.app',
       viewCode: 'https://github.com/branthajek/todo-list-react-ts',
@@ -68,7 +68,7 @@ export class ProjectCardsComponent {
     },
     { 
       title: 'Weather App', 
-      description: 'Using plain ES6 Javascript, I created a simple weather app using the OpenWeatherMap API. You can enter a city and get the current weather conditions for that location.',
+      description: 'This is a very simple weather app made with plain Javascript, using the OpenWeatherMap API. You can enter a city and get the current weather conditions for that location.',
       codeDescription: 'Javascript, OpenWeatherMap API',
       viewBrowser: 'https://js-weather-app-5cf87.web.app',
       viewCode: 'https://github.com/branthajek/weather-app',
